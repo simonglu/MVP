fbe64c613979efaba1a22d2afdd5318fd84cc89e
Simon <simonglu2@gmail.com>
2021-07-07 12:38:40 -0700

    Seeded file

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const axios = require('axios');
+let app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+app.use(express.static(__dirname + '/../client/dist'));
+
+let PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
+  console.log(`listening on port ${PORT}`);
+});
